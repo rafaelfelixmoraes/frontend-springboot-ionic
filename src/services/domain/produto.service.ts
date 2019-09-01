ccefd53e8de9aebe7282e1f16a41505200bdb54e
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2019-09-01 16:03:05 -0300

    Carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Observable } from "rxjs/Rx";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService{
@@ -9,9 +10,19 @@ export class ProdutoService{
     constructor(public httpClient: HttpClient){
     }
 
+    findById(produto_id : string){
+        return this.httpClient.get<ProdutoDTO>(
+            `${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id : string) {
         return this.httpClient.get(
             `${API_CONFIG.baseUrl}/produtos/page/?categorias=${categoria_id}`);
+    }
+
+    getImageFromCloudinary(id : String) : Observable<any>{
+        let url = `${API_CONFIG.cloudinaryBaseUrl}/products/prod${id}.jpg`;
+        return this.httpClient.get(url, {responseType: 'blob'});
     }
 
     getSmallImageFromCloudinary(id : String) : Observable<any>{
