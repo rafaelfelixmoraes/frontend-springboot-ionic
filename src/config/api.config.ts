8db1b5c3e46c429dd017eae591d099dba1a0101f
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2020-05-25 18:41:47 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://stylefelix-spring-course.herokuapp.com",
     amazonS3BucketBaseUrl: "https://stylefelix-spring-ionic.s3-sa-east-1.amazonaws.com"
 }
